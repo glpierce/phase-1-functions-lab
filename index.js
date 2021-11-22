e19d73ddec02b358864db6b2d9505a01872c89d3
Grant Pierce <glpierce174@gmail.com>
2021-11-22 17:36:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blockDistance = someValue - 42;
+    if (blockDistance < 0) {
+        blockDistance = blockDistance * -1;
+    }
+    return blockDistance;
+  }
+
+function distanceFromHqInFeet(someValue) {
+    let blockDistance = distanceFromHqInBlocks(someValue);
+    return blockDistance * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blockDistance = start - destination;
+    if (blockDistance < 0) {
+        blockDistance = blockDistance * -1;
+    }
+    return blockDistance * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    if (feetDistance <= 400) {
+        return 0;
+    } else if (400 < feetDistance && feetDistance <= 2000) {
+        return (2 * (feetDistance - 400)) / 100;
+    } else if (2000 < feetDistance && feetDistance <= 2500) {
+        return 25;
+    } else if (feetDistance > 2500) {
+        return `cannot travel that far`;
+    }
+  }
